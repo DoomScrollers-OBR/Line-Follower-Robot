31d8ec39af03de4aa52bfb90a5781eab4e687966
JoaoLucasSerafim <joaoserafim615@gmail.com>
2026-06-27 20:33:24 -0300

    adicionei o display da imagem e o controlador

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2 as cv
 import RPi.GPIO as GPIO
-import time
+from time import time
 
 Window_name = "Seguidor de Linha"
 
-def image_process():
+def image_process(frame):
 
     height, width = frame.shape[:2]     # Obtem a altura e largura da imagem
     roi = frame[height // 2:height, :]  # Utiliza a região de interesse, que é a metade inferior da imagem
@@ -19,10 +19,10 @@ def image_process():
             )                           # Binariza a imagem, deixando em preto e branco de forma invertida (Branco vira preto e preto vira branco)
 
     contours, _ = cv.findContours(
-            binary
+            binary,
             cv.RETR_EXTERNAL,
             cv.CHAIN_APPROX_SIMPLE
-            )                           # Procura os contornos
+                )                           # Procura os contornos
 
                                         
     cx = None                           # Definem o centro da imagem no eixo X e Y
@@ -41,7 +41,7 @@ def image_process():
 
             cv.drawContours(
                     roi,
-                    [maior],
+                    [bigger],
                     -1,
                     (0,255,0),
                     2
@@ -53,7 +53,60 @@ def image_process():
                     (0, 0, 255),
                     -1
                     )                               # Desenha o centro da linha
+            
+            center = width // 2                     # Centro da Imagem
 
-            return frame, roi, binary, cx, cy, width, height
+            cv.line(
+                 frame,
+                 (center, 0),
+                 (center, height),
+                 (255, 0, 0),
+                 2
+            )
 
+            if cx is not None:
+                 error = cx - center
+
+            return frame, roi, binary, cx, cy, width, height, center, error
+
+
+def display():
+    cam = cv.VideoCapture(0)
+
+    if not cam.IsOpened():
+        print("Não foi possivel abrir a câmera")
+        return
+    cv.namedWindow(Window_name, cv.WINDOW_NORMAL)
+
+    while True:
+
+        ret, frame = cam.read()
+
+        if not ret:
+             break
+
+        frame, roi, binary, cx, cy, width, height, center, error = image_process(frame)
+
+        cv.imshow(Window_name, frame)
+
+        if cv.waitkey(1) & 0xFF == 27:
+                break
+
+        return cam
+
+
+def controlador():
+     
+     frame, roi, binary, cx, cy, width, height, center, error = image_process(frame)
+
+     if error >= center-20:
+        print("Turn Left")
+     elif error <= center-20:
+        print("Turn Right")
+     elif error == center:
+        print("On Track")
+
+cam = display()
+cam.release()
+cv.destroyAllWindows
 
